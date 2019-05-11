da080470740dba02d30334686a8d46c32eed590b
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 00:39:42 +0100

    trying to fix lambda iam role

--- deploy/modules/lambda/lambda.tf
+++ deploy/modules/lambda/lambda.tf
@@ -13,18 +13,18 @@ resource "aws_iam_role" "lambda_exec" {
   name = "${var.function_name}-role"
 
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
