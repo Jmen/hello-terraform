b360712793253c4d1a4dc8e5b6d9d79a1e19b9bb
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 15:04:13 +0100

    Adding proxy resource to api gateway

--- deploy/modules/api-gateway/api-gateway.tf
+++ deploy/modules/api-gateway/api-gateway.tf
@@ -20,6 +20,31 @@ resource "aws_api_gateway_integration" "root_integration" {
     uri                     = "${var.lambda_invoke_arn}"
 }
 
+resource "aws_api_gateway_resource" "proxy_resource" {
+    rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+
+    parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+    path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy_method" {
+    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+    resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
+
+    http_method   = "ANY"
+    authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "proxy_integration" {
+    rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+    resource_id = "${aws_api_gateway_method.proxy_method.resource_id}"
+
+    http_method             = "${aws_api_gateway_method.proxy_method.http_method}"
+    integration_http_method = "POST"
+    type                    = "AWS_PROXY"
+    uri                     = "${var.lambda_invoke_arn}"
+}
+
 resource "aws_lambda_permission" "lambda_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
