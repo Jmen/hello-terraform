d1367e52fc81b24bc03dd610dc1445bb472bbca2
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 17:11:23 +0100

    reverting permissions

--- deploy/modules/lambda/lambda.tf
+++ deploy/modules/lambda/lambda.tf
@@ -14,18 +14,17 @@ resource "aws_iam_role" "lambda_exec" {
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "*"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 EOF
 }
