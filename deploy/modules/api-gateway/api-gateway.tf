5f460b602588fa59a39adcd5b81e3e1c1549286f
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 01:16:38 +0100

    trying to move lambda proxy to root of api gateway

--- deploy/modules/api-gateway/api-gateway.tf
+++ deploy/modules/api-gateway/api-gateway.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_resource" "resource" {
 
 resource "aws_api_gateway_method" "method" {
     rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.resource.id}"
+    resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
     http_method   = "ANY"
     authorization = "NONE"
 }
