69b65d8e126c4d3ee51c0f0fc9ac13ae09b98df6
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 16:51:32 +0100

    trying to fix lambda logging permissions

--- deploy/modules/lambda/lambda.tf
+++ deploy/modules/lambda/lambda.tf
@@ -14,26 +14,18 @@ resource "aws_iam_role" "lambda_exec" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "*"
-    }
-  ]
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
 }
 EOF
 }
