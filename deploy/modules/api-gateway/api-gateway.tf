8139304ce08ce403aa7abaf329fcb9c189c9d5ff
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 02:20:27 +0100

    simplifying api gateway (hopefully)

--- deploy/modules/api-gateway/api-gateway.tf
+++ deploy/modules/api-gateway/api-gateway.tf
@@ -2,31 +2,6 @@ resource "aws_api_gateway_rest_api" "api" {
     name = "${var.name}"
 }
 
-resource "aws_api_gateway_resource" "proxy_resource" {
-    rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-
-    parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-    path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy_method" {
-    rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
-
-    http_method   = "ANY"
-    authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "proxy_integration" {
-    rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-    resource_id = "${aws_api_gateway_method.proxy_method.resource_id}"
-
-    http_method             = "${aws_api_gateway_method.proxy_method.http_method}"
-    integration_http_method = "POST"
-    type                    = "AWS_PROXY"
-    uri                     = "${var.lambda_invoke_arn}"
-}
-
 resource "aws_api_gateway_method" "root_method" {
     rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
     resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
