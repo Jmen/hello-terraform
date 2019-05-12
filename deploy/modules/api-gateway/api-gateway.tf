1fd311df16576361ee303802191fefdfd1d8b388
Jaimen Lathia <Jaimen.Lathia@moonpig.com>
2019-05-12 01:29:23 +0100

    trying to move lambda proxy to root of api gateway

--- deploy/modules/api-gateway/api-gateway.tf
+++ deploy/modules/api-gateway/api-gateway.tf
@@ -2,26 +2,29 @@ resource "aws_api_gateway_rest_api" "api" {
     name = "${var.name}"
 }
 
-resource "aws_api_gateway_resource" "resource" {
+resource "aws_api_gateway_resource" "proxy" {
     rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+
     parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
     path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "request_method" {
     rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-    resource_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+
+    resource_id   = "${aws_api_gateway_resource.proxy.id}"
     http_method   = "ANY"
     authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "integration" {
+resource "aws_api_gateway_integration" "request_method_integration" {
     rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-    resource_id = "${aws_api_gateway_resource.resource.id}"
-    http_method = "${aws_api_gateway_method.method.http_method}"
+    
+    resource_id = "${aws_api_gateway_resource.proxy.id}"
+    http_method = "${aws_api_gateway_method.request_method.http_method}"
+    type        = "AWS_PROXY"
+    uri         = "${var.lambda_invoke_arn}"
     integration_http_method = "POST"
-    type                    = "AWS_PROXY"
-    uri                     = "${var.lambda_invoke_arn}"
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
